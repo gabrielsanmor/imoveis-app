d6355e4d4f939ffeea3dad09f635cb3841ba41ae
gabriel.moreno <gsantiag.info@gmail.com>
2022-12-19 15:56:40 -0400

    Inicio cadastro componente

--- client/ImoveisFrontEnd/src/app/components/cadastro/cadastro.component.ts
+++ client/ImoveisFrontEnd/src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,7 +10,20 @@ export class CadastroComponent implements OnInit {
 
   constructor() { }
 
+  cadastro: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    nome: new FormControl(''),
+    sobrenome:new FormControl(''),
+    email: new FormControl(''),
+    senha: new FormControl(''),
+    senha2: new FormControl(''),
+  });
+
   ngOnInit(): void {
+  }
+
+  submit(){
+
   }
 
 }
